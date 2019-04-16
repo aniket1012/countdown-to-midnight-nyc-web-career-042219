f82d4e2ae35b3eaaf257134d433703273185be57
Aniket Patel <aniketupatel1@gmail.com>
2019-04-16 20:24:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,10 +10,8 @@ def countdown(num)
 end
 
 def countdown_with_sleep(num)
-  while num > 0
-    puts "#{num} SECOND(S)!"
-   num -= 1
+  puts "#{num} SECOND(S)!"
   sleep 1
-  num -= 5
+  num -= 1
 end
 "HAPPY NEW YEAR!"
